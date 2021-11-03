ee7dc9fa6fc5781f6f3a40e946617a596b8bb607
soblab <soblab@WHIT208W10_12.it.farmingdale.edu>
2021-11-03 10:00:17 -0400

    Overloading example

---
+++ src/main/java/com/mycompany/week_10/rectangle.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.week_10;
+
+/**
+ *
+ * @author soblab
+ */
+public class rectangle extends shapes{
+    private int width;
+    private int height;
+    
+    public void setHeight(int h){
+        height=h;
+    }
+    public void setWidth(int width){
+        this.width=width;
+    }
+    public void setBrdrWidth(int b){
+        
+    }
+}
---
+++ src/main/java/com/mycompany/week_10/shapes.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.week_10;
+
+/**
+ *
+ * @author soblab
+ */
+public class shapes {
+    int brdrW;
+    String brdrColor;
+    
+    public shapes(){      
+        brdrW=10;
+        brdrColor="Blue";
+     
+    }
+    public shapes(int b, String c){      
+        brdrW=10;
+        brdrColor="Blue";
+     
+    }
+    
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+           
+    }
+    
+    
+        
+        
+   
+       
+        
+}
